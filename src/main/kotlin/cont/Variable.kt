c844fdd0ac660822792a968bb6591b2160a8a894
choejeonghwan <yzon007@naver.com>
2023-08-09 16:26:43 +0900

    [add] variable

---
+++ src/main/kotlin/cont/Variable.kt
@@ -0,0 +1,33 @@
+var x = 5
+
+fun main() {
+
+    //val(value) 값 변경 불가능
+    //var(variable) 값 변경 가능
+
+    x += 1;
+
+    val a : Int = 1;
+
+    val b = 1;
+
+    val c : Int
+    c = 3
+
+    val d : Int
+    d = 123
+
+    var e : String = "Hello";
+    e = "yes"
+
+    var f = 123
+    f = 123
+
+    println(a);
+    println(b);
+    println(c);
+    println(e);
+    println(x);
+
+
+}

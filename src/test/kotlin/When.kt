81053e93731e50db634b7894631dc755da794698
choejeonghwan <yzon007@naver.com>
2023-08-29 09:35:19 +0900

    [add] null safety

--- src/test/kotlin/When.kt
+++ src/test/kotlin/When.kt
@@ -2,6 +2,16 @@ package test.kotlin
 
 fun main() {
     // 자바코드를 코틀린의 when 식으로 변환한 코드
+    val day = 2
+
+    val result = when(day){
+        1 -> "월요일"
+        2->"화요일"
+        3->"수요일"
+        4->"목요일"
+        else -> "기타"
+    }
+    println("result: $result")
 
     //else를 생략할 수 있다
 
@@ -9,3 +19,8 @@ fun main() {
 
 
 }
+
+enum class Color {
+    RED, GREEN
+}
+
